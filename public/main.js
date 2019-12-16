41a584e9cd72c1ef1dcfb941ea1c7c514334fb12
Frazer MacRostie <frazermac101@outlook.com>
2019-12-16 14:48:39 +0000

    CSS Styling

--- public/main.js
+++ public/main.js
@@ -1,1 +1,23 @@
-//////HI GUYS
+const hbs = require('hbs');
+const path = require('path');
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const publicDirectory = path.join(__dirname, "../public");
+
+app.use(express.static(publicDirectory));
+
+
+app.use(bodyParser.json());
+// app.engine('.hbs',hbs({    defaultLayout: 'layout',    extname: '.hbs'}));
+
+app.set('view engine','.hbs');
+
+app.get('/', async(req, res) => {
+    res.render('index', );
+    });
+    app.post('/',async(req,res) => {
+});
+app.listen(3000, () => {
+        console.log('Your Sever is running on Port 3000')
+    })
